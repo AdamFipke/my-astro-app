d6e777d49b3c83dc9bdd2f8d0f0641dd05432698
Adam Fipke <adamfipke@gmail.com>
2025-05-06 18:18:17 -0700

    add more stuff from my config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from "@tailwindcss/vite"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +11,19 @@ export default defineConfig({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled. See https://github.com/withastro/astro/issues/12824#issuecomment-2563095382
+      alias: import.meta.env.PROD
+        ? {
+            "react-dom/server": "react-dom/server.edge",
+          }
+        : undefined,
+    },
+  },
+  integrations: [react()],
+  prefetch: true,
 });
